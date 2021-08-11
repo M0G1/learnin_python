6989bbea016baa8bec5b21bd953ae607ae29c2a2
M0G1 <44916652+M0G1@users.noreply.github.com>
2021-08-12 00:01:09 +0400

    corrected some mistake

--- regular_expression/mag_exam.py
+++ regular_expression/mag_exam.py
@@ -4,11 +4,10 @@ python 3.9
 качестве входного параметра строку, в которой производится замена всех
 первых букв после точек на заглавные
 """
-
 import re
 
-ss = ".a .b asdgasd.e"
-pattern = re.compile(r"\.[a-z]")
+ss = ".a .b asdgasd.e . aasdf"
+pattern = re.compile(r"\.([^A-Za-z])*[a-z]")
 
 
 def upper_match(s: re.Match):
@@ -18,16 +17,20 @@ def upper_match(s: re.Match):
 def up_letter_after_dot(s: str):
     return re.sub(pattern, upper_match, s)
 
-
 def up_letter_after_dot_simple(s: str):
-    ans = s[0]
-    for i in range(1, len(s)):
-        if s[i - 1] == "." and ("a" <= s[i] <= "z"):
-            ans = ans + s[i].capitalize()
-        else:
-            ans = ans + s[i]
-    return ans
-
+    answer = ""
+    ii = 0
+    while ii < len(s):
+        if s[ii] == '.':
+            while ii < len(s) and not s[ii].isalpha():
+                answer = answer + s[ii]
+                ii = ii + 1
+            answer = answer + s[ii].upper()
+            ii = ii + 1
+            continue
+        answer = answer + s[ii]
+        ii = ii + 1
+    return answer
 
 print("use re: ", up_letter_after_dot(ss))
 print("simple: ", up_letter_after_dot_simple(ss))
