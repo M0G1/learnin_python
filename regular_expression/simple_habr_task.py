cc4098f49ff37b604393494dc2d85992cc052312
M0G1 <44916652+M0G1@users.noreply.github.com>
2021-08-16 13:14:26 +0400

    new task have completed

--- regular_expression/simple_habr_task.py
+++ regular_expression/simple_habr_task.py
@@ -66,5 +66,90 @@ PS. С отношением 25:50 всё нормально!"""
     print(ans)
 
 
+def task5():
+    inpu = """1.2 
+  1. 
+    1.0e-55  
+      e-12   
+  6.5E 
+        1e-12  
+  +4.1234567890E-99999           
+  7.6e+12.5 
+   99 """
+
+    pattern = re.compile(r"[+-]?\d+(?:(?:\.\d+)|(?:\.\d+)?(?:[eE][+-]?\d+))")
+
+    for s in inpu.split():
+        m = re.fullmatch(pattern, s)
+        if m is not None:
+            print(s, "is legal")
+        else:
+            print(s, "is illegal")
+
+
+def task6():
+    inpu = """Это курс информатики соответствует ФГОС и ПООП, 
+это подтверждено ФГУ ФНЦ НИИСИ РАН"""
+
+    pattern = re.compile(r"[А-Я]{2,}(?:\s[А-Я]{2,})*")
+
+    res = re.findall(pattern, inpu)
+    print(len(res), type(res))
+    for ii in res:
+        print("\t", ii, "\t", type(ii), len((ii)))
+
+
+def task7():
+    inpu = """Было закуплено 12 единиц техники 
+по 410.37 рублей"""
+
+    pattern = re.compile(r"\d+")
+
+    def match_handle(m: re.Match):
+        digit = int(m.group())
+        digit = digit ** 3
+        return str(digit)
+
+    ans = re.sub(pattern, match_handle, inpu)
+    print(ans)
+
+
+def task8():
+    inpu = ("""Московский государственный институт международных отношений""",
+            """микоян авиацию снабдил алкоголем, 
+народ доволен работой авиаконструктора""")
+
+    pattern = re.compile(r"\b[А-Яа-яёЁ]")
+    for ss in inpu:
+        ans = ("".join(re.findall(pattern, ss))).upper()
+        print(ans)
+
+
+def task9():
+    inpu = ("Вечер за окном. / Еще один день прожит. / Жизнь скоротечна...",
+            "Просто текст",
+            "Как вишня расцвела! / Она с коня согнала / И князя-гордеца.",
+            "На голой ветке / Ворон сидит одиноко… / Осенний вечер!",
+            "Тихо, тихо ползи, / Улитка, по склону Фудзи, / Вверх, до самых высот!",
+            "Жизнь скоротечна… / Думает ли об этом / Маленький мальчик.")
+
+    pattern = re.compile(r"[ёЁуУеЕыЫаАоОэЭяЯиИюЮ]")
+
+    syllable = (5, 7, 5)
+    for ss in inpu:
+        splitted = ss.split("/ ")
+        if len(splitted) != 3:
+            print("Не хайку. Должно быть 3 строки.")
+            continue
+
+        for ii in range(3):
+            count = len(re.findall(pattern, splitted[ii]))
+            if count != syllable[ii]:
+                print(f"Не хайку. В {ii + 1} строке слогов не {syllable[ii]}, а {count}.")
+                break
+        else:
+            print("Хайку!")
+
+
 if __name__ == '__main__':
-    task4()
+    task9()
